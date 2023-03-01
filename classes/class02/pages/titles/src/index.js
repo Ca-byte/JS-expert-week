37f3fe4c3a75844bb05747f329647e383f101f36
Caroline Vieira <carolinesanvieira@gmail.com>
2023-03-01 03:07:51 +0100

    docs: class 02

---
+++ classes/class02/pages/titles/src/index.js
@@ -0,0 +1,5 @@
+import cardsFactory from "./factories/cardsFactory.js"
+import HandGestureFactory from "./factories/handGestureFactory.js"
+
+await cardsFactory.initalize()
+await HandGestureFactory.initalize()
